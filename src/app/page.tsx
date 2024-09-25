69649c4106b444ff944b941925e17706169621a6
suriyasg <suriyasg@gmail.com>
2024-09-25 17:57:36 +0530

    updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Hero from "./components/Home/Hero";
+import CategoryComponent from "./components/Home/Category";
+import TrendingProducts from "./components/Home/TrendingProducts";
 
 export default function Home() {
   return (
-    <p>Home Page</p>
+    <>
+      <Header isLoggedIn={true}/>
+      <Nav />
+      <Hero/>
+      <CategoryComponent/>
+      <TrendingProducts/>
+    </>
   );
 }
