4e843112afea0a250e936b3119062aaddca3cde8
suriyasg <suriyasg@gmail.com>
2024-10-08 19:50:54 +0530

    feat(api): returning trending product properly

--- src/app/api/home/getproducts/route.ts
+++ src/app/api/home/getproducts/route.ts
@@ -1,37 +1,31 @@
 import { RowDataPacket } from "mysql2/promise";
 import { pool } from "../../dbconnect";
-// TODO : Write correct logic for get recommened_products and trending_products
+// TODO : Write correct logic for get recommened_products
 interface Product {
     ProductID: number;
     ProductTitle: string;
     BasePrice: number;
     PrimaryImage: string; // URL to the product image
 }
-interface SearchKey {
-    ProductID: number;
-    ProductTitle: string;
-}
 interface response {
     status: number;
     message?: string;
     recommened_products: Product[];
     trending_products: Product[];
-    available_products: SearchKey[];
 }
 
 export async function GET(req: any) {
     const conn = await pool.getConnection();
     try {
-        const trending_query = `SELECT ProductID, ProductTitle, BasePrice, PrimaryImage FROM BaseProduct`;
-        const available_products_query = `SELECT ProductID, ProductTitle FROM BaseProduct`;
+        const trending_query = `SELECT ProductID, ProductTitle, BasePrice, PrimaryImage FROM TrendingProducts`;
+        const recommened_products_query = `SELECT ProductID, ProductTitle, BasePrice, PrimaryImage FROM BaseProduct`;
         const [trending_products_result] = await conn.execute<RowDataPacket[]>(trending_query);
-        const [available_products_result] = await conn.execute<RowDataPacket[]>(available_products_query);
+        const [recommened_products_result] = await conn.execute<RowDataPacket[]>(recommened_products_query);
         const data: response = {
             status: 200,
             message: "success",
-            recommened_products: trending_products_result as Product[],
+            recommened_products: recommened_products_result as Product[],
             trending_products: trending_products_result as Product[],
-            available_products: available_products_result as SearchKey[], 
         }
 
         const myHeader = new Headers();
