b86d11031ace6ef816bbb80f3865e3b72177faff
suriyasg <suriyasg@gmail.com>
2024-10-08 19:53:17 +0530

    fix: sending proper response when MYySQL server is down

--- src/app/api/home/searchproducts/route.ts
+++ src/app/api/home/searchproducts/route.ts
@@ -1,4 +1,4 @@
-import { RowDataPacket } from "mysql2/promise";
+import { PoolConnection, RowDataPacket } from "mysql2/promise";
 import { pool } from "../../dbconnect";
 
 interface SearchKey {
@@ -12,15 +12,16 @@ interface response {
 }
 
 export async function GET(req: any) {
-    const conn = await pool.getConnection();
+    let conn: PoolConnection | null = null;
     try {
+        conn = await pool.getConnection();
         const available_products_query = `SELECT ProductID, ProductTitle FROM BaseProduct`;
         const [available_products_result] = await conn.execute<RowDataPacket[]>(available_products_query);
         // TODO : handle when available_products_result is empty
         const data: response = {
             status: 200,
             message: "success",
-            available_products: available_products_result as SearchKey[], 
+            available_products: available_products_result as SearchKey[],
         }
 
         const myHeader = new Headers();
@@ -31,14 +32,25 @@ export async function GET(req: any) {
         })
         return response;
     }
-    catch (error) {
-        console.log(error)
-        return new Response(JSON.stringify({ message: 'Internal Server Error' }), {
-            status: 500,
-            headers: { 'Content-Type': 'application/json' },
-        });
+    catch (error: any) {
+        console.log("this is fateal : ", error)
+        if (error.code === 'ECONNREFUSED') {
+            console.error('Database server is down:', error.message);
+            return new Response(JSON.stringify({ status: 503, message: 'Database server is down, please try again later.' }), {
+                status: 503,
+                headers: { 'Content-Type': 'application/json' },
+            });
+        } else {
+            console.error('An unexpected error occurred:', error.message);
+            return new Response(JSON.stringify({ status: 500, message: 'Internal Server Erorr, please try again later.' }), {
+                status: 500,
+                headers: { 'Content-Type': 'application/json' },
+            });
+        }
     }
     finally {
-        pool.releaseConnection(conn);
+        if (conn) {
+            pool.releaseConnection(conn);
+        }
     }
 }
