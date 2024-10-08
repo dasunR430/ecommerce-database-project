f8680f3c06fa6d4f05e901b50b5b1ac8f3c88579
suriyasg <suriyasg@gmail.com>
2024-10-08 20:17:26 +0530

    chore: changed variables names according to api

--- src/app/components/Home/CategorySection.tsx
+++ src/app/components/Home/CategorySection.tsx
@@ -2,48 +2,50 @@ import Link from 'next/link';
 import React from 'react';
 
 interface SubCategory {
-    id: number;
-    name: string;
+    SubCategoryID: number;
+    SubCategoryName: string;
+    MainCategoryID: number;
 }
 
-interface MainCategory {
+interface Category {
     id: number;
     name: string;
     subCategories: SubCategory[];
 }
+interface response {
+    status: number;
+    message?: string;
+    categories: Category[];
+}
 
-const categories: MainCategory[] = [
-    {
-        id: 1,
-        name: 'Electronics',
-        subCategories: [
-            { id: 1, name: 'Mobile Phones' },
-            { id: 2, name: 'Laptops' },
-            { id: 3, name: 'Televisions' },
-        ],
-    },
-    {
-        id: 2,
-        name: 'Toys',
-        subCategories: [
-            { id: 4, name: 'Action Figures' },
-            { id: 5, name: 'Board Games' },
-            { id: 6, name: 'Dolls' },
-        ],
-    },
-];
+const CategorySection: React.FC = async () => {
+    
+    const fetchProcducts = async () => {
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/home/getcategories`);
+            if (response.status !== 200)
+                throw Error("response not ok!!")
+            else {
+                const data : response = await response.json();
+                return data.categories;
+            }
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+    let categories : Category[] | undefined = await fetchProcducts();
 
-const CategorySection: React.FC = () => {
     return (
         <div className="p-4">
             <h2 className="text-2xl font-bold mb-4">Categories</h2>
             <div className="">
                 <ul className="mt-2">
-                    {categories.map((category) => (
+                    {categories?.map((category) => (
                         category.subCategories.map((subCategory) => (
-                            <Link href={`/category/${category.name.toLowerCase()}/${subCategory.name.toLowerCase()}`}>
-                                <li key={subCategory.id} className="p-4 m-2 hover:bg-gray-100 text-gray-700 inline-block shadow-md rounded-md border border-gray-200 hover:text-blue-600 cursor-pointer">
-                                    {subCategory.name}
+                            <Link href={`/category/${category.name.toLowerCase()}/${subCategory.SubCategoryName.toLowerCase()}`}>
+                                <li key={subCategory.SubCategoryID} className="p-4 m-2 hover:bg-gray-100 text-gray-700 inline-block shadow-md rounded-md border border-gray-200 hover:text-blue-600 cursor-pointer">
+                                    {subCategory.SubCategoryName}
                                 </li>
                             </Link>
                         ))
