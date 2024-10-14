96e2127ab526d5484973e9edd418230221b07414
suriyasg <suriyasg@gmail.com>
2024-10-14 17:44:43 +0530

    feat: incrementing click count when view product button clicked

---
+++ src/app/api/home/incrementClickCount/route.ts
@@ -0,0 +1,38 @@
+import { RowDataPacket } from "mysql2/promise";
+import { pool } from "../../dbconnect";
+
+interface response {
+    status: number;
+    message: string;
+}
+
+export async function POST(req: any) {
+    const conn = await pool.getConnection();
+    try {
+        const {productID} = await req.json();
+        const incrementing_query = `INSERT INTO ClickCount (ProductID, ClickDate) VALUES (?, CONCAT(CURDATE(), ' 01:00:00')) ON DUPLICATE KEY UPDATE ClickDate = VALUES(ClickDate),  Count = Count + 1;`;
+        const [incrementing_query_result] = await conn.execute<RowDataPacket[]>(incrementing_query, [productID]);
+        const data: response = {
+            status: 200,
+            message: "success",
+        }
+
+        const myHeader = new Headers();
+        myHeader.append('Content-Type', 'application/json')
+        const response: Response = new Response(JSON.stringify(data), {
+            status: 200,
+            headers: myHeader,
+        })   
+        return response;
+    }
+    catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({ message: 'Internal Server Error' }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+    finally {
+        pool.releaseConnection(conn);
+    }
+}
--- src/app/components/Home/Products.tsx
+++ src/app/components/Home/Products.tsx
@@ -1,5 +1,7 @@
-import { error } from 'console';
-import React, { useEffect } from 'react';
+'use client'
+
+import { useRouter } from "next/navigation";
+
 interface Product {
   ProductID: number;
   ProductTitle: string;
@@ -9,10 +11,23 @@ interface Product {
 
 interface ProductsProps {
   products: Product[];
-  heading : string;
+  heading: string;
 }
 
-const Products: React.FC<ProductsProps>= ({products, heading}) => {
+const Products: React.FC<ProductsProps> = ({ products, heading }) => {
+  const router = useRouter();
+
+  const handleOnClick = async (productID: number) => {
+    await fetch(`${process.env.NEXT_PUBLIC_API_URL}/home/incrementClickCount`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ productID: productID })
+      }
+    );
+    router.push(`/products/${productID}`)
+  }
+  
   return (
     <div className="p-4">
       <h2 className="text-2xl font-bold mb-4">{heading}</h2>
@@ -24,7 +39,10 @@ const Products: React.FC<ProductsProps>= ({products, heading}) => {
               <h3 className="text-xl font-semibold mt-2">{product.ProductTitle}</h3>
               <p className="text-lg text-gray-700">${product.BasePrice}</p>
               <div className="mt-auto align-baseline"> {/* This pushes the button to the bottom */}
-                <button className="bg-white text-red-800 border border-red-800 py-2 px-4 rounded w-full justify-between items-center hover:bg-red-800 hover:text-white transition-colors duration-300">
+                <button
+                  className="bg-white text-red-800 border border-red-800 py-2 px-4 rounded w-full justify-between items-center hover:bg-red-800 hover:text-white transition-colors duration-300"
+                  onClick={() => { handleOnClick(product.ProductID) }}
+                >
                   View Details
                 </button>
               </div>
