cde94c9e0c8ccba0a16078b375cc8e696543c69f
Prabath Samarasinghe <prabhaths2001@gmail.com>
2024-10-22 11:09:13 +0530

    Product details page and the cart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import Image from "next/image";
 
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
+  const routeto = () => {
+    router.push("/route/prabath/details");
+  }
   return (
-    <p>Home Page</p>
+    <>
+      <p>Home Page</p>
+      <button onClick={routeto}>Cart</button>
+    </>
   );
 }
