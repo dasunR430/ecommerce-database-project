4b5a8a929f3e3739d8233cdacc0f439688a74a07
HimalaGunathilaka <157564894+HimalaGunathilaka@users.noreply.github.com>
2024-10-23 21:17:40 +0530

    Contact Details Profile
    Now contact details in profile will show customer's saved contact details.

--- src/app/api/profile/getContactDetails/route.ts
+++ src/app/api/profile/getContactDetails/route.ts
@@ -1,6 +1,45 @@
-import {pool} from '@/sharedCode/dbconnect';
+import { getToken } from "next-auth/jwt";
+import { NextRequest } from "next/server";
+import mysql from 'mysql2/promise';
+import { pool } from '@/sharedCode/dbconnect';
 
 
-export async function GET(req: any) {
-    const connection = pool.getConnection();
+interface ContactDetails {
+    AddressLine1: string;
+    AddressLine2: string;
+    City: string;
+    District: string;
+    PostalCode: string;
+}
+
+export async function POST(req: NextRequest) {
+    // const token = await getToken({ req });
+
+    const requestBody = await req.json();
+
+    const id = requestBody.id;
+
+    try {
+        const connection = await pool.getConnection();
+
+        const retriveQuery = 'SELECT AddressLine1,AddressLine2,City,District,PostalCode from contactdetails WHERE CustomerID = ?';
+        const [rows] = await connection.execute<mysql.RowDataPacket[]>(retriveQuery, [id]);
+
+        const contacts: ContactDetails[] = rows as ContactDetails[];
+
+        connection.release();
+
+        // console.log(purchases);
+
+        return new Response(JSON.stringify(contacts), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (error) {
+        console.error('Failed to get customer purchases:', error);
+        return new Response(JSON.stringify({ message: 'Internal server error' }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
 }
