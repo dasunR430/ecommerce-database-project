11c39dfecbcd7555882e9ca7d5b812d5f395b1ce
Prabath Samarasinghe <prabhaths2001@gmail.com>
2024-10-24 19:47:23 +0530

    select address added to cart summery

--- src/app/cartsummery/page.js
+++ src/app/cartsummery/page.js
@@ -5,11 +5,13 @@ import AddressInfo from "./components/addressinfo.js";
 import Paymentdetails from "./components/paydetails.js";
 import ShipingDetails from "./components/shipingdetails.js";
 import Additional from "./components/additional.js";
+import SelectAddress from "./components/selectadd.js";
 
 export default function CartSummery(){
     return(
         <div className={styles.summerybody}>
-            <div className={styles.sumbodyright}>               
+            <div className={styles.sumbodyright}>  
+                <SelectAddress />
                 <Address />
                 <ShipingDetails />
             </div>
