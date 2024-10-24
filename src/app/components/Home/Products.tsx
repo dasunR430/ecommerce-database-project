2e1dfcd774025b15b295a54c628cc0947c7ee65a
suriyasg <suriyasg@gmail.com>
2024-10-24 20:32:06 +0530

    added images for base products

--- src/app/components/Home/Products.tsx
+++ src/app/components/Home/Products.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useRouter } from "next/navigation";
-
+import Image from 'next/image';
 interface Product {
   ProductID: number;
   ProductTitle: string;
@@ -31,46 +31,49 @@ const Products: React.FC<ProductsProps> = ({ products, heading }) => {
   return (
     <div className="p-4">
       <h2 className="text-2xl font-bold mb-4">{heading}</h2>
-      <div className="overflow-x-auto">
-        <div className="flex space-x-4">
-          {products.map((product) => (
-            <div key={product.ProductID} className="min-w-[200px] border rounded-lg p-4 bg-white shadow-md flex flex-col">
-              <img src={'/'} alt={product.ProductTitle} className="h-48 w-full object-cover rounded-md" />
-              <h3 className="text-xl font-semibold mt-2">{product.ProductTitle}</h3>
-              <p className="text-lg text-gray-700">{formatPrice(product.BasePrice)}</p>
-              <div className="mt-auto align-baseline"> {/* This pushes the button to the bottom */}
-                <button
-                  className="bg-white text-red-800 border border-red-800 py-2 px-4 rounded w-full justify-between items-center hover:bg-red-800 hover:text-white transition-colors duration-300"
-                  onClick={() => { handleOnClick(product.ProductID) }}
-                >
-                  View Details
-                </button>
-              </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"> {/* Use grid layout */}
+        {products.map((product) => (
+          <div key={product.ProductID} className="max-w-[300px] border rounded-lg p-4 bg-white shadow-md flex flex-col">
+            <Image
+              src={product.PrimaryImage}
+              alt={product.ProductTitle}
+              width={200}
+              height={300}
+              className="w-full h-auto object-cover" // Make sure image covers the space
+            />
+            <h3 className="text-xl font-semibold mt-2">{product.ProductTitle}</h3>
+            <p className="text-lg text-gray-700">{formatPrice(product.BasePrice)}</p>
+            <div className="mt-auto align-baseline"> {/* This pushes the button to the bottom */}
+              <button
+                className="bg-white text-red-800 border border-red-800 py-2 px-4 rounded w-full justify-between items-center hover:bg-red-800 hover:text-white transition-colors duration-300"
+                onClick={() => { handleOnClick(product.ProductID) }}
+              >
+                View Details
+              </button>
             </div>
-          ))}
-        </div>
-        {
-          products.length == 0 ?
-            (
-              <div className="text-center text-gray-500 py-6">
-                <p className="text-lg font-medium">
-                  No {heading} Available at the moment
-                </p>
-              </div>
-            )
-            : <></>
-        }
+          </div>
+        ))}
       </div>
+      {
+        products.length === 0 ? (
+          <div className="text-center text-gray-500 py-6">
+            <p className="text-lg font-medium">
+              No {heading} Available at the moment
+            </p>
+          </div>
+        ) : null
+      }
     </div>
+
   );
 };
 
 export default Products;
 
-function formatPrice(amount : number, currency : string = 'LKR') {
+function formatPrice(amount: number, currency: string = 'LKR') {
   return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: currency,
-      minimumFractionDigits: 2
+    style: 'currency',
+    currency: currency,
+    minimumFractionDigits: 2
   }).format(amount);
 }
