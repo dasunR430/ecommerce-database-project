54c71b76bcc0b50eb01eed62e449984645740e36
suriyasg <suriyasg@gmail.com>
2024-10-24 10:37:01 +0530

    removed headerprops

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header isLoggedIn={false} />
+        <Header />
         {children}
         <Footer />
       </body>
